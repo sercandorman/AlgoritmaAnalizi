05826bb6a8e9ebf357f0ed59f8ef18248d4d9fa9
Sercan Dorman <sercandorman21@hotmail.com>
2017-03-03 13:02:32 +0200

    Add files via upload

---
+++ pow_lineer_compx_O_n/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int sayac,x,y,sonuc=1,i=0;
+
+int recursive_pow_cmplx_O_n(int x, int y){
+    for (i=1;i<y+1;i++){
+        sonuc=sonuc*x;
+        sayac=sayac+1;
+        }
+        return sonuc;
+    }
+    
+int main(int argc, char *argv[])
+{
+  printf("x : ");
+  scanf("%d",&x);
+  printf("y : ");
+  scanf("%d",&y);
+  sonuc = recursive_pow_cmplx_O_n(x,y);
+  printf("Sonuc : %d \nAdim Sayisi = %d \n",sonuc,sayac);
+  
+  system("PAUSE");	
+  return 0;
+}
